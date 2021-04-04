876676ad1bd30a867bf73945add7c4ce56e3d515
Pai026 <abhirampai1999@gmail.com>
2021-04-04 18:40:00 +0530

    added image upload option

--- app/db.py
+++ app/db.py
@@ -2,6 +2,9 @@ import pymongo
 import os
 from pymongo import MongoClient
 from dotenv import dotenv_values, find_dotenv
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 config = dotenv_values(find_dotenv())
 MongoURL = os.environ.get("MONGO_URL") or config["MONGO_URL"]
@@ -12,3 +15,8 @@ cluster = MongoClient(MongoURL)
 db = cluster[db_config]
 user_collection = db[user_collection_config]
 image_collection = db[image_collection_config]
+cloudinary.config( 
+  cloud_name = config["CLOUDNAME"], 
+  api_key = config["CLOUDINARYAPI"], 
+  api_secret = config["CLOUDINARYAPISECRET"] 
+)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from .routes.user_route import router as UserRouter
 from fastapi.middleware.cors import CORSMiddleware
+from .db import cloudinary
 
 app = FastAPI()
 
@@ -19,6 +20,13 @@ app.add_middleware(
 app.include_router(UserRouter, tags=["User"], prefix="/user")
 
 
-@app.get("/", tags=["Root"])
+@app.get("/")
 def read_root():
     return {"Message": "Welcome to DermaCam Backend End !!!"}
+
+@app.post("/uploadImage",response_description="Return Image Url")
+async def uploadImage(image: UploadFile = File(...)):
+    result = cloudinary.uploader.upload(image.file)
+    return {
+        'data':result['url']
+        }
