a4061816d18e923b59e61e69b6cb5e3eb8844aea
Pai026 <abhirampai1999@gmail.com>
2021-04-04 13:03:01 +0530

    changes in db config

--- app/db.py
+++ app/db.py
@@ -4,7 +4,11 @@ from pymongo import MongoClient
 from dotenv import dotenv_values, find_dotenv
 
 config = dotenv_values(find_dotenv())
-cluster = MongoClient(os.environ.get("MONGO_URL"))
-db = cluster[os.environ.get("DATABASE")]
-user_collection = db[os.environ.get("USERS")]
-image_collection = db[os.environ.get("IMAGES")]
+MongoURL = os.environ.get("MONGO_URL") or config["MONGO_URL"]
+db_config = os.environ.get("DATABASE") or config["DATABASE"] 
+user_collection_config =  os.environ.get("DATABASE") or config["DATABASE"] 
+image_collection_config = os.environ.get("IMAGES") or config["IMAGES"] 
+cluster = MongoClient(MongoURL)
+db = cluster[db_config]
+user_collection = db[user_collection_config]
+image_collection = db[image_collection_config]
