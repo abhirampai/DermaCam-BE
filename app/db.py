e23d4e9bb2d9602bcabb31efdbf235292853df95
Pai026 <abhirampai1999@gmail.com>
2021-04-04 13:09:44 +0530

    fixes

--- app/db.py
+++ app/db.py
@@ -6,7 +6,7 @@ from dotenv import dotenv_values, find_dotenv
 config = dotenv_values(find_dotenv())
 MongoURL = os.environ.get("MONGO_URL") or config["MONGO_URL"]
 db_config = os.environ.get("DATABASE") or config["DATABASE"] 
-user_collection_config =  os.environ.get("DATABASE") or config["DATABASE"] 
+user_collection_config =  os.environ.get("USERS") or config["USERS"] 
 image_collection_config = os.environ.get("IMAGES") or config["IMAGES"] 
 cluster = MongoClient(MongoURL)
 db = cluster[db_config]
