50736ebc1337f5b1c287ca67cb152e525e19f45f
Pai026 <abhirampai1999@gmail.com>
2021-04-04 12:41:11 +0530

    added cors middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from .routes.user_route import router as UserRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+    "https://dermacam-be.herokuapp.com/",
+    "http://dermacam-be.herokuapp.com/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(UserRouter, tags=["User"], prefix="/user")
 
