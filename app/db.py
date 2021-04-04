467c067a66c039af94165e72a1d6dad68709eabe
Pai026 <abhirampai1999@gmail.com>
2021-04-04 12:28:14 +0530

    added os environ to get heroku envs

--- app/db.py
+++ app/db.py
@@ -1,10 +1,11 @@
 import pymongo
+import os
 from pymongo import MongoClient
 from dotenv import dotenv_values, find_dotenv
 
 config = dotenv_values(find_dotenv())
 
-cluster = MongoClient(config["MONGO_URL"])
-db = cluster[config["DATABASE"]]
-user_collection = db[config["USERS"]]
-image_collection = db[config["IMAGES"]]
+cluster = MongoClient(config["MONGO_URL"] or os.environ.get("MONGO_URL"))
+db = cluster[config["DATABASE"] or os.environ.get("DATABASE")]
+user_collection = db[config["USERS"] or os.environ.get("USERS")]
+image_collection = db[config["IMAGES"] or os.environ.get("IMAGES")]
