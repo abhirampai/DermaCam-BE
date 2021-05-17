20d26ab1d294f4859b853e32b3d7e74e04752da5
Pai026 <abhirampai1999@gmail.com>
2021-05-17 13:15:45 +0530

    added detection model

--- app/routes/user_route.py
+++ app/routes/user_route.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, File, UploadFile
 from ..schemas.RegisterUserSchema import RegisterUserSchema, RegisterResponseModel, ResetPasswordSchema
 from ..schemas.PatientHealthSchema import PatientHealthSchema, GetHealthStatus
 from ..schemas.LoginUserSchema import LoginUserSchema, LoginResponseModel, GetUserResponseModel, ResetPasswordResponseModel
 from ..services import (user_service, auth_service,)
+from ..db import cloudinary
 
 router = APIRouter()
 auth_service = auth_service.AuthHandler()
@@ -41,3 +42,9 @@ def get_status(userid=Depends(auth_service.auth_wrapper)):
 @router.get('/userHealthDetail', response_description="Get user's health details")
 def get_user_health_details(userid=Depends(auth_service.auth_wrapper)):
     return user_service.get_user_health_detail(userid)
+
+
+@router.post('/diseaseDetail', response_description="Get user's facial disease")
+def get_user_disease_detail(image: UploadFile = File(...)):
+    result = cloudinary.uploader.upload(image.file)
+    return user_service.get_user_disease_detail(result['url'])
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -3,8 +3,15 @@ from .auth_service import AuthHandler
 from fastapi.encoders import jsonable_encoder
 from fastapi import HTTPException
 from bson.objectid import ObjectId
-
-
+from keras.models import load_model
+from keras.preprocessing import image
+import urllib.request
+import cv2
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+import numpy as np
+model_file = "app/model/DiseaseDetectionModel.h5"
+severity_file = "app/model/SeverityModel.h5"
 auth_service = AuthHandler()
 
 
@@ -112,3 +119,35 @@ def get_user_health_detail(userid):
             }
     except:
         raise HTTPException(status_code=401, detail='Health details not entered')
+
+def get_user_disease_detail(imageUrl):
+    detectionModel = load_model(model_file)
+    resp = urllib.request.urlopen(imageUrl)
+    imageUploaded = np.asarray(bytearray(resp.read()), dtype="uint8")
+    test_image = cv2.imdecode(imageUploaded, cv2.IMREAD_COLOR)
+    resized_shape = (64,64)
+    test_img = cv2.resize(test_image,(resized_shape[1],resized_shape[0]))
+    skinDiseaseTypes=['blackhead', 'Acne', 'kutil filiform', 'flek hitam', 'folikulitis', 'milia', 'Dermatitis perioral', 'Karsinoma', 'panu', 'melanoma', 'herpes', 'Eksim', 'papula', 'whitehead', 'Tinea facialis', 'rosacea', 'Pustula', 'psoriasis']
+    x = image.img_to_array(test_img)
+    x = np.expand_dims(x, axis = 0)
+
+    x /= 255
+
+    custom = detectionModel.predict(x)
+    x = np.array(x, 'float32')
+    a=custom[0]
+    ind=np.argmax(a)
+    if(skinDiseaseTypes[ind] == 'Acne'):
+        severityModel = load_model(severity_file)
+        severityLevel=['Level_0', 'Level_2', 'Level_1']
+        custom = severityModel.predict(x)
+        x = np.array(x, 'float32')
+        a=custom[0]
+        index=np.argmax(a)
+        return {
+        "Prediction":skinDiseaseTypes[ind],
+        "SeverityLevel":severityLevel[index]
+        }
+    return {
+        "Prediction":skinDiseaseTypes[ind]
+        }
